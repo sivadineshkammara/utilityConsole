80be038b76a04bac58d5ffbad403a8093c54f416
sivadineshkammara <sivadineshkammara@gmail.com>
2026-08-03 23:17:14 +0200

    module info for geometry updated

---
+++ include/geometry.h
@@ -0,0 +1,22 @@
+/*
+ * ============================================================================
+ * Module      : Geometry
+ * Responsibility:
+ *   - Find area and perimeter of various geometric shapes
+ *   - Provide reusable geometry APIs for other modules
+ *
+ * Public API:
+ *   findArea()
+ *   findPerimeter()
+
+ *  Private Helpers:
+ *    areaOfSquare()
+ *    areaOfRectangle()
+ *    areaOfTriangle()
+ *    areaOfCircle()
+ *    perimeterOfSquare()
+ *    perimeterOfRectangle()
+ *    perimeterOfTriangle()
+ *    perimeterOfCircle()
+ * ============================================================================
+ */
