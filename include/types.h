0a01d9601f7f7d80c7c99003e4be3269a951525e
Siva Dinesh Kammara <cy04bopo@cip115-60.e-technik.uni-erlangen.de>
2026-08-08 20:03:08 +0200

    enum vals added for all options in the code

---
+++ include/types.h
@@ -0,0 +1,35 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef enum{
+    MENU_CALCULATOR = 1,
+    MENU_TEMPERATURE,
+    MENU_GEOMETRY,
+    MENU_EXIT
+}MainMenuOption;
+
+typedef enum{
+    OP_ADD = 1,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_MODULUS,
+    OP_EXIT
+}Operation;
+
+typedef enum{
+    SHAPE_SQUARE = 1,
+    SHAPE_RECTANGLE,
+    SHAPE_TRIANGLE,
+    SHAPE_CIRCLE,
+    SHAPE_EXIT
+} Shape;
+
+typedef enum{
+    SCALE_CELSIUS = 1,
+    SCALE_FAHRENHIET,
+    SCALE_KELVIN,
+    SCALE_EXIT
+}TemperatureScale;
+
+#endif
